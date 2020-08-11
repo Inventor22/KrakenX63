9b07a4e583831dc9d180f6241e67e1277a59f845
Dustin Dobransky <dd2_inventor_climber@hotmail.com>
2020-08-11 16:59:27 -0700

    Ported support for SuperFixed, SuperBreathing, and Wings effects

---
+++ KrakenX63Tests/Program.cs
@@ -0,0 +1,45 @@
+﻿using KrakenX63Driver;
+using System;
+using System.Drawing;
+
+namespace KrakenX63Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            KrakenX63 x63 = new KrakenX63();
+
+            x63.SetColor(
+                KrakenX63.KrakenXColorChannel.Ring,
+                KrakenX63.ColorEffect.SuperFixed,
+                new Color[] {
+                    Color.FromArgb(0, 255, 255), // cyan
+                    Color.FromArgb(255, 0, 180), // pink
+                    Color.FromArgb(0, 255, 255),
+                    Color.FromArgb(255, 0, 180),
+                    Color.FromArgb(0, 255, 255),
+                    Color.FromArgb(255, 0, 180),
+                    Color.FromArgb(0, 255, 255),
+                    Color.FromArgb(255, 0, 180)
+                },
+                KrakenX63.AnimationSpeed.Slower);
+
+            x63.SetColor(
+                KrakenX63.KrakenXColorChannel.Logo,
+                KrakenX63.ColorEffect.SuperBreathing,
+                new Color[] {
+                    Color.FromArgb(255, 0, 180)
+                },
+                KrakenX63.AnimationSpeed.Slower);
+
+            //x63.SetColor(
+            //    KrakenX63.KrakenXColorChannel.Ring, 
+            //    KrakenX63.ColorEffect.Wings,
+            //    new Color[] { Color.FromArgb(255, 0, 0) },
+            //    KrakenX63.AnimationSpeed.Normal);
+        }
+    }
+}
